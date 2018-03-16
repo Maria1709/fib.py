6986b495b6747db06bbd37d7bb0de91b46c5d131
Maria1709 <G00364700@gmit.ie>
2018-03-16 15:42:22 +0000

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -2,4 +2,4 @@
 # Maria o sullivan
 with open("data/iris.csv") as f:
       for line in f: 
-print('{:5d} {:5d} {:5d} {:5d}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+print('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
