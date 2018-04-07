1a10d81af0661e14dc383a6103a8af8d947796ec
Maria1709 <G00364700@gmit.ie>
2018-04-07 14:22:18 +0100

    Update function 1.py

--- function 1.py
+++ function 1.py
@@ -2,6 +2,7 @@
 # using function to find a factorial
 def factorial(n):
     result = 1
+    #goes through every iteration until it gets to 1
     for i in range(1,n +1):
         result*=i
     return result
