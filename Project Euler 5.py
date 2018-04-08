7135f38af05dbd867621b98a933423b68e3126b6
Maria1709 <G00364700@gmit.ie>
2018-04-08 21:36:57 +0100

    Update Project Euler 5.py

--- Project Euler 5.py
+++ Project Euler 5.py
@@ -6,6 +6,7 @@
 i = 20
 while 1:   
     i+=20
+    #i remainder 11 equals 0
     if i%11==0 and i%12==0 and i%13==0 and i%14==0 and i%15==0\
     and i%15==0 and i%16==0 and i%17==0 and i%18==0 and i%19==0:
         print(i)
